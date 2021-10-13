c2dade5a903bd29e165462add7511c3ef53cca75
schau298 <schau298@mail.nmc.edu>
2021-10-13 00:21:42 -0400

    Add project files.

---
+++ RoutingPractice/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoutingPractice.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("[action]/{start}/{end?}/{message?}")]
+        public IActionResult Countdown(int start, int end = 0, string message = "")
+        {
+            string contentString = "Counting down: \n";
+            for (int i = start; i >= end; i--)
+            {
+                contentString += i + "\n" + message;
+            }
+            return Content(contentString);
+        }
+        public IActionResult Index()
+        {
+            return Content("Home");
+        }
+
+        public IActionResult Privacy()
+        {
+            return Content("Privacy");
+        }
+
+        public IActionResult Display(string id)
+        {
+            if (id == null)
+            {
+                return Content("No ID supplied.");
+            }
+            else
+            {
+                return Content("ID: " + id);
+            }
+        }
+    }
+}
